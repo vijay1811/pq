a270328bad381c9e12619dd47f08cf94700b0538
vijay1811 <vijay1811.kumar@gmail.com>
2019-09-20 07:39:27 +0530

    Treat op error as bad connection

--- error.go
+++ error.go
@@ -494,7 +494,7 @@ func (cn *conn) errRecover(err *error) {
 		}
 	case *net.OpError:
 		cn.bad = true
-		*err = v
+		*err = driver.ErrBadConn
 	case error:
 		if v == io.EOF || v.(error).Error() == "remote error: handshake failure" {
 			*err = driver.ErrBadConn
